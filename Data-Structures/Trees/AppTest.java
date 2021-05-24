a6e8e35df47b97946e4274fdebac35f42bd7428c
Osama Abo Oura <34076117+semo4@users.noreply.github.com>
2021-05-25 01:25:38 +0300

    Update AppTest.java

--- Data-Structures/Trees/AppTest.java
+++ Data-Structures/Trees/AppTest.java
@@ -152,4 +152,93 @@ public class AppTest {
         exp.add("8");
         assertEquals("the tree is Buzz",exp ,f.fizzBuzzTree(b).preOrder());
     }
+  
+    @Test
+    public void testTreeIntersectionEmpty(){
+        BinaryTree test = new BinaryTree();
+        BinaryTree b = new BinaryTree();
+        BinaryTree t = new BinaryTree();
+
+        assertNull("the tree empty",test.tree_Intersection(t,b));
+    }
+
+    @Test
+    public void testTreeIntersectionEqualTree(){
+        BinaryTree test = new BinaryTree();
+        BinaryTree t = new BinaryTree();
+            t.add(150);
+            t.add(100);
+            t.add(250);
+            t.add(75);
+            t.add(160);
+            t.add(200);
+            t.add(350);
+            t.add(125);
+            t.add(175);
+            t.add(300);
+            t.add(500);
+
+        BinaryTree b = new BinaryTree();
+            b.add(42);
+            b.add(100);
+            b.add(600);
+            b.add(15);
+            b.add(160);
+            b.add(200);
+            b.add(350);
+            b.add(125);
+            b.add(175);
+            b.add(4);
+            b.add(500);
+
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(100);
+        a.add(125);
+        a.add(160);
+        a.add(200);
+        a.add(175);
+        a.add(350);
+        a.add(500);
+
+
+        assertEquals("the tree full",a,test.tree_Intersection(t,b));
+    }
+
+    @Test
+    public void testTreeIntersectionNotEqualTree(){
+        BinaryTree test = new BinaryTree();
+        BinaryTree t = new BinaryTree();
+        t.add(150);
+        t.add(100);
+        t.add(250);
+        t.add(75);
+        t.add(160);
+        t.add(200);
+        t.add(350);
+        t.add(125);
+        t.add(175);
+        t.add(300);
+        t.add(500);
+
+        BinaryTree b = new BinaryTree();
+        b.add(42);
+        b.add(100);
+        b.add(600);
+        b.add(15);
+        b.add(160);
+        b.add(200);
+        b.add(350);
+        b.add(125);
+
+
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(100);
+        a.add(125);
+        a.add(160);
+        a.add(200);
+        a.add(350);
+
+
+        assertEquals("the tree full",a,test.tree_Intersection(t,b));
+    }
 }
