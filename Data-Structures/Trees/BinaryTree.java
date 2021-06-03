9a71503b44342116b88171ceb4f9678f9621923d
Osama Abo Oura <34076117+semo4@users.noreply.github.com>
2021-06-03 10:38:50 +0300

    Update BinaryTree.java

--- Data-Structures/Trees/BinaryTree.java
+++ Data-Structures/Trees/BinaryTree.java
@@ -139,6 +139,20 @@ public class BinaryTree<T> extends BinarySearchTree{
         }
         return s;
     }
+  
+     public boolean isSymmetric(TreeNode root) {
+        return mirror(root, root);    
+    }
+    public boolean mirror(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        return (left.val == right.val) && mirror(left.left, right.right) && mirror(left.right, right.left); 
+   
+    }
 
 }
 
