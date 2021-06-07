4ad69d1c2372a58e28db2345bb785c9c54565333
Osama Abo Oura <34076117+semo4@users.noreply.github.com>
2021-06-07 17:02:30 +0300

    Update BinaryTree.java

--- Data-Structures/Trees/BinaryTree.java
+++ Data-Structures/Trees/BinaryTree.java
@@ -142,9 +142,11 @@ public class BinaryTree<T> extends BinarySearchTree{
   
   // qusetion solved tranning 
   
-     public boolean isSymmetric(Node root) {
-        return mirror(root, root);    
+    // question that i added
+    public boolean isSymmetric(Node root) {
+        return mirror(root, root);
     }
+
     public boolean mirror(Node left, Node right){
         if(left == null && right == null){
             return true;
@@ -152,88 +154,89 @@ public class BinaryTree<T> extends BinarySearchTree{
         if(left == null || right == null){
             return false;
         }
-        return (left.data == right.data) && mirror(left.left, right.right) && mirror(left.right, right.left); 
-   
+        return (left.data == right.data) && mirror(left.left, right.right) && mirror(left.right, right.left);
+
     }
-  
-   public boolean isValidBST(Node root) {
-        
-         return isBST(root,null,null ) ;
+
+    public boolean isValidBST(Node root) {
+
+        return isBST(root,null,null ) ;
     }
-    
+
     public boolean isBST(Node root, Node left, Node right){
         if (root == null)
             return true;
-         if (left != null && root.data <=left.data)
+        if (left != null && (Integer)root.data <=(Integer)left.data)
             return false;
-        if (right != null && root.data >= right.data)
+        if (right != null && (Integer)root.data >= (Integer)right.data)
             return false;
-        
+
         return isBST(root.left, left, root) &&isBST(root.right, root, right);
     }
-  
-      public int maxDepth(Node root) {
-        if (root == null) 
-          return 0; 
 
-    // Get the depth of the left and right subtree 
-    // using recursion.
-        int leftDepth = maxDepth(root.left); 
-        int rightDepth = maxDepth(root.right); 
+    public int maxDepth(Node root) {
+        if (root == null)
+            return 0;
 
-    // Choose the larger one and add the root to it.
-       if (leftDepth > rightDepth) 
-        return (leftDepth + 1); 
-      else 
-       return (rightDepth + 1); 
-     
+        // Get the depth of the left and right subtree
+        // using recursion.
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        // Choose the larger one and add the root to it.
+        if (leftDepth > rightDepth)
+            return (leftDepth + 1);
+        else
+            return (rightDepth + 1);
+
     }
-  public List<List<Integer>> levelOrder(TreeNode root) {
+
+    public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> values = new ArrayList<List<Integer>>();
         if(root ==null){
             return values;
         }
         if(root!= null){
-            Queue<TreeNode> queue = new LinkedList<TreeNode>();
+            Queue<Node> queue = new LinkedList<Node>();
             queue.add(root);
-           
+
             List<Integer> items = new ArrayList<>();
-            items.add(root.val);
-             values.add(items);
+            items.add((Integer)root.data);
+            values.add(items);
             while(!queue.isEmpty()){
-                TreeNode curr = queue.poll();
-                 List<Integer> item = new ArrayList<>();
+               Node curr = queue.poll();
+                List<Integer> item = new ArrayList<>();
 
                 if(curr.left != null){
-                     queue.add(curr.left);
-                     item.add(curr.left.val);
+                    queue.add(curr.left);
+                    item.add((Integer)curr.left.data);
                 }
 
-                 if(curr.right != null){
-                     queue.add(curr.right);
-                      item.add(curr.right.val);
+                if(curr.right != null){
+                    queue.add(curr.right);
+                    item.add((Integer)curr.right.data);
                 }
                 if(!item.isEmpty()){
-                     values.add(item);
+                    values.add(item);
                 }
 
 
             }
 
-            
-        }
-       return values;
-    }
-  
-    public int treeSum(Node curr){
-          if(curr == null){
-              return 0;
-          }
-          int leftSum = treeSum(curr.left);
-          int rightSum = treeSum(curr.right);
 
-          return ((Integer)curr.data + leftSum+ rightSum);
-      }
+        }
+        return values;
+    }
+
+    public int treeSum(Node curr){
+        if(curr == null){
+            return 0;
+        }
+        int leftSum = treeSum(curr.left);
+        int rightSum = treeSum(curr.right);
+
+        return ((Integer)curr.data + leftSum+ rightSum);
+    }
 
     public  boolean inTree(Node curr, int value){
         if(curr== null){
@@ -282,7 +285,7 @@ public class BinaryTree<T> extends BinarySearchTree{
 
         return (Integer)rootOne.data == (Integer)rootTwo.data && isSameTree(rootOne.left, rootTwo.left) && isSameTree(rootOne.right, rootTwo.right);
     }
-  
+
     public Node mergeTrees(Node t1, Node t2){
         if(t1== null & t2!=null){
             t1=t2;
@@ -296,7 +299,8 @@ public class BinaryTree<T> extends BinarySearchTree{
         if(t1== null & t2!=null){
             t1=t2;
         }else if (t1 != null & t2 !=null){
-            (Integer)t1.data += (Integer)t2.data;
+           int  newValue = ((Integer)t1.data + (Integer)t2.data);
+            t1.data  = newValue;
             merge(t1.left, t2.left);
             merge(t1.right, t2.right);
 
@@ -308,7 +312,8 @@ public class BinaryTree<T> extends BinarySearchTree{
             }
         }
     }
-  
+
+
     public void zigzagPrint(Node root){
         if(root == null){
             return;
@@ -363,6 +368,196 @@ public class BinaryTree<T> extends BinarySearchTree{
 
         }
         return true;
+    }
+
+   public int countLeaf(){
+        return countLeaves(root);
+   }
+
+   public int countLeaves(Node root){
+        if(root == null){
+            return 0;
+        }
+        if(root.isLeaf()){
+            return 1;
+        }
+        return countLeaves(root.left) + countLeaves(root.right);
+   }
+
+   public boolean isFullBinaryTree(Node root){
+        if(root == null){
+            return true;
+        }
+        else if(root.left ==null && root.right == null){
+            return true;
+        }
+        return isFullBinaryTree(root.left) && isFullBinaryTree(root.right);
+
+   }
+
+   public int minDepth(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if(root.left == null){
+            return left + 1;
+        }
+       if(root.right == null){
+           return right + 1;
+       }
+
+       if(left<right){
+           return left + 1;
+       }else{
+           return right + 1;
+       }
+   }
+
+   public boolean isIdentical(Node rootOne, Node rootTwo){
+        if(rootOne== null && rootTwo == null){
+            return true;
+        }
+        return (rootOne!=null && rootTwo!=null) && (rootOne.data == rootTwo.data) &&
+                isIdentical(rootOne.left, rootTwo.left)
+                && isIdentical(rootOne.right, rootTwo.right);
+   }
+
+   public int convertInPlace(Node root){
+        if(root ==null){
+            return 0;
+        }
+
+        int left = convertInPlace(root.left);
+        int right = convertInPlace(root.right);
+
+        int temp = (Integer)root.data;
+        root.data = left + right;
+
+        return (Integer)root.data + temp;
+
+   }
+
+   public int isSumTree(Node root){
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return (Integer)root.data;
+        }
+        if((Integer)root.data == isSumTree(root.left) + isSumTree(root.right)){
+            return 2* (Integer)root.data;
+        }
+
+        return Integer.MIN_VALUE;
+   }
+
+   public void pathToLeaf(Node root){
+        Deque<Integer> path = new ArrayDeque<>();
+        printPathToLeaf(root, path);
+   }
+
+   public void printPathToLeaf(Node root,  Deque<Integer> path ){
+        if(root == null){
+            return;
+        }
+        path.addLast((Integer)root.data);
+
+        if(root.isLeaf()){
+            System.out.println(path);
+        }
+       printPathToLeaf(root.left, path);
+       printPathToLeaf(root.right, path);
+
+       path.removeLast();
+   }
+
+   public boolean printAncestors(Node root, int value){
+        if(root == null){
+            return false;
+        }
+        if((Integer)root.data == value){
+            return true;
+        }
+
+        boolean left = printAncestors(root.left, value);
+
+        boolean right = false;
+        if(!left){
+            right = printAncestors(root.right, value);
+        }
+        if(left||right){
+            System.out.println(root.data+" ");
+        }
+        return left || right;
+   }
+
+   public void diagonalSum(Node root){
+        Map<Integer,Integer> map = new HashMap<>();
+       diagonalSum(root, 0, map);
+       System.out.println(map.values());
+   }
+
+   public void diagonalSum(Node root, int d,  Map<Integer,Integer> map){
+        if(root==null){
+            return;
+        }
+        map.put(d, map.getOrDefault(d,0)+ (Integer)root.data);
+       diagonalSum(root.left, d+1, map);
+       diagonalSum(root.right, d, map);
+   }
+
+   public void snikNodes(Node root){
+        if(root == null){
+            return;
+        }
+        snikNodes(root.left);
+        snikNodes(root.right);
+
+        if((Integer)root.data==0){
+            sink(root);
+        }
+    }
+
+   public void sink(Node root){
+        if(root == null){
+            return;
+        }
+        if(root.left!= null && (Integer)root.left.data !=0){
+            int temp = (Integer)root.data;
+            root.data = root.left.data;
+            root.left.data = temp;
+            sink(root.left);
+        }
+        else  if(root.right!= null && (Integer)root.right.data !=0){
+            int temp = (Integer)root.data;
+            root.data = root.right.data;
+            root.right.data = temp;
+            sink(root.right);
+        }
+    }
+
+   public boolean isBalanced(Node root){
+        AtomicBoolean isBalance = new AtomicBoolean(true);
+        isHeightBalanced(root, isBalance);
+        return isBalance.get();
+    }
+
+    public int isHeightBalanced(Node root, AtomicBoolean isBalance){
+        if(root == null || isBalance.get()){
+            return 0;
+        }
+
+        int left_height = isHeightBalanced(root.left, isBalance);
+        int right_height = isHeightBalanced(root.right, isBalance);
+
+        if(Math.abs(left_height - right_height) > 1){
+            isBalance.set(false);
+        }
+        return Math.max(left_height, right_height)+1;
     }
 
 
